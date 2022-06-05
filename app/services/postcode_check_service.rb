28af6258e9a484dbfa79ea2ef25e3ba9a66e9d30
Kara Potts <karaken12@gmail.com>
2022-06-05 22:14:08 +0100

    Simplify postcode check code further

--- app/services/postcode_check_service.rb
+++ app/services/postcode_check_service.rb
@@ -2,20 +2,24 @@ class PostcodeCheckService
   def shippable?(postcode)
     postcode = postcode.upcase.gsub(/[^A-Z\d]/, '')
 
-    return true if ADDITIONAL_SHIPPABLE_POSTCODES.include?(postcode)
-
-    lsoa = get_lsoa(postcode)
-    return true if SHIPPABLE_AREAS.any? do |area|
-      lsoa.start_with?("#{area} ")
-    end
-
-    false
+    in_additional_shippable_list?(postcode) || in_shippable_area?(postcode)
   end
 
   private
 
   ADDITIONAL_SHIPPABLE_POSTCODES = %w[SH241AA SH241AB].freeze
   SHIPPABLE_AREAS = %w[Southwark Lambeth].freeze
+
+  def in_additional_shippable_list?(postcode)
+    ADDITIONAL_SHIPPABLE_POSTCODES.include?(postcode)
+  end
+
+  def in_shippable_area?(postcode)
+    lsoa = get_lsoa(postcode)
+    SHIPPABLE_AREAS.any? do |area|
+      lsoa.start_with?("#{area} ")
+    end
+  end
 
   def get_lsoa(postcode)
     case postcode
