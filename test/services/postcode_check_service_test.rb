f7ab8eff4b2d47602e75b58c231f4f125fbb7dea
Kara Potts <karaken12@gmail.com>
2022-06-05 23:01:09 +0100

    Clean up the postcode check service tests a little

--- test/services/postcode_check_service_test.rb
+++ test/services/postcode_check_service_test.rb
@@ -1,46 +1,34 @@
 require 'test_helper'
-# require 'minitest/mock'
 
 class PostcodeCheckServiceTest < ActiveSupport::TestCase
+  setup do
+    @service = PostcodeCheckService.new
+  end
+
   test 'should allow a postcode in Southwark' do
-    service = PostcodeCheckService.new
-    postcode = 'SE1 7QD'
-    assert_equal(true, service.shippable?(postcode))
+    assert_equal(true, @service.shippable?('SE1 7QD'))
   end
 
   test 'should allow a postcode in Lambeth' do
-    service = PostcodeCheckService.new
-    postcode = 'SE1 7QA'
-    assert_equal(true, service.shippable?(postcode))
+    assert_equal(true, @service.shippable?('SE1 7QA'))
   end
 
   test 'should not allow a postcode in York' do
-    service = PostcodeCheckService.new
-    postcode = 'YO10 5DD'
-    assert_equal(false, service.shippable?(postcode))
+    assert_equal(false, @service.shippable?('YO10 5DD'))
   end
 
   test 'should allow explicitly added postcodes' do
-    service = PostcodeCheckService.new
-    postcode = 'SH24 1AA'
-    assert_equal(true, service.shippable?(postcode))
+    assert_equal(true, @service.shippable?('SH24 1AA'))
   end
 
   test 'should allow badly formatted postcodes' do
-    service = PostcodeCheckService.new
-    postcode = '   Se17qD   '
-    assert_equal(true, service.shippable?(postcode))
-    postcode = "sE1\t7Qa\n"
-    assert_equal(true, service.shippable?(postcode))
-    postcode = ' s H 2 4 1 A A '
-    assert_equal(true, service.shippable?(postcode))
+    assert_equal(true, @service.shippable?('   Se17qD   '))
+    assert_equal(true, @service.shippable?("sE1\t7Qa\n"))
+    assert_equal(true, @service.shippable?(' s H 2 4 1 A A '))
   end
 
   test 'should not allow invalid postcodes' do
-    service = PostcodeCheckService.new
-    postcode = ''
-    assert_equal(false, service.shippable?(postcode))
-    postcode = 'invalid'
-    assert_equal(false, service.shippable?(postcode))
+    assert_equal(false, @service.shippable?(''))
+    assert_equal(false, @service.shippable?('invalid'))
   end
 end
