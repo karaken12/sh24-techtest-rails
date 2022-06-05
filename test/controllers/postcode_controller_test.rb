f140d9e687d80be51d59c46583ca1fd4281f9eae
Kara Potts <karaken12@gmail.com>
2022-06-05 20:10:10 +0100

    Add basic, non-functional form

--- test/controllers/postcode_controller_test.rb
+++ test/controllers/postcode_controller_test.rb
@@ -6,4 +6,18 @@ class PostcodeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_match 'Postcode', @response.body
   end
+
+  test 'should show a basic form' do
+    get root_url
+    assert_select 'form', { count: 1 } do |elements|
+      form_element = elements[0]
+      assert_equal('/search', form_element['action'])
+      assert_equal('get', form_element['method'])
+
+      assert_select 'input', { count: 2 } do |inputs|
+        assert_equal('text', inputs[0]['type'])
+        assert_equal('submit', inputs[1]['type'])
+      end
+    end
+  end
 end
